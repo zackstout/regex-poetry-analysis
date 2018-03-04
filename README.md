54cd79d141950c327cfe17d1487cc3f64a3e63a7
Zack Stout <zackstout@gmail.com>
2018-03-04 00:49:57 -0600

    adding readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Poetry Analysis with Regex
+Learning regex by doing some poetry analysis using [PoetryDB](http://poetrydb.org/index.html). Starting with counting instances of a user-inputted word in all of an author's poems, hoping to scale up to looking at all authors. Immediate goal: save the data to a local database so we don't have to wait on the API every time.
--- client.js
+++ client.js
@@ -4,8 +4,34 @@ var count = 0;
 
 $(document).ready(function() {
 
+
+
+
+  // Get list of authors:
+  $.ajax({
+    type: 'get',
+    url: "http://poetrydb.org/authors"
+  }).done(function(data) {
+    console.log(data);
+
+    data.authors.forEach(function(auth) {
+      $.ajax({
+        type: 'get',
+        url: "http://poetrydb.org/author/" + auth
+      }).done(function(res) {
+        console.log(res);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    });
+
+  });
+
+  // Get number of a given word in that author's works:
   $('#sub').on('click', function() {
     var author = $('#author').val();
+    var term = $('#searchTerm').val();
+
     $.ajax({
       type: "get",
       // url: "http://poetrydb.org/author,title/Shakespeare;Sonnet"
@@ -13,11 +39,15 @@ $(document).ready(function() {
     }).done(function(data) {
       console.log(data);
       count = 0;
+      // console.log(term);
 
       // This works as intended:
       data.forEach(function(poem) {
         poem.lines.forEach(function(line) {
-          var regex = /[lL]ove/g;
+          // var regex = /[lL]ove/g;
+          // var regex = / + term + /g;
+          var regex = new RegExp(term);
+          // console.log(regex);
           if ( regex.test(line) ) {
           // if (line.includes('love') || line.includes('Love')) {
             count ++;
@@ -25,8 +55,8 @@ $(document).ready(function() {
         });
       });
       console.log(count);
-
-
+      $('#search').text(term);
+      $('#count').text(count);
 
     }).catch(function(err) {
       console.log(err);
